b964f8c06619de495e8ba132204a1aeee9110eb8
Tanner Foster <tannerfoster@ferristexas.gov>
2025-02-07 13:07:21 -0600

    Arrow Functions Lab Submission

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 
+function createLoginTracker() {
+
+  const userInfo = {
+    username: "user1",
+    password: "password123"
+  }
+  let attemptCount = 0;
+
+const handleLogin = (passwordAttempt) => {
+
+  if(attemptCount <= 1){
+  if(passwordAttempt == userInfo.password){
+    return("Login Successful");
+  }else{
+    return("Login Failed");
+    attemptCount = attemptCount + 1;
+    handleLogin(prompt("Password:"));
+  }
+}else {
+  return("Account locked due to too many failed login attempts")
+}
+
+
+
+
+}
+
+handleLogin(prompt("Password:"));
+
+}
+
+createLoginTracker();
 
 
 module.exports = {
